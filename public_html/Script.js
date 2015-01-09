6e3168672ba0d28ae55cac31cf7dfb838d39d990
javier alvarez <javi_612@yahoo.com>
2015-01-09 14:01:25 -0800

    portfolio

--- public_html/Script.js
+++ public_html/Script.js
@@ -5,10 +5,29 @@
  */
 
 
-$("document").ready(function(){
-    $(".practice").css("background-color", "blue");
+$('document').ready(function(){
+    $(".yeah").css("background-color", "blue");
       
-    $("div:nth-child(1)").css("background-color", "pink");
+    //$("div:nth-child(1)").css("background-color", "pink");
     
-    $("div p:last-child").css("background-color", "cyan")
+    //$("div p:last-child").css("background-color", "cyan");
+    
+    $("h1").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
+    
+    $('div').bind('mouseover', mouseOverMe());
+    
+    $('h1').bind('click', mouseClick);
 });
+
+function mouseOverMe(){
+    $("p").html("hairline");
+}
+
+function mouseOutMe(){
+    $('h1').html('an');
+}
+
+function mouseClick(){
+    
+    $('p').html('click');
+}
