7f0aaa42633f168d1f6b777ff4dc109219f0ba4e
javier alvarez <javi_612@yahoo.com>
2015-01-13 14:03:37 -0800

    portfolio

--- public_html/Script.js
+++ public_html/Script.js
@@ -16,14 +16,37 @@ $('document').ready(function(){
     
     $('div').bind('mouseover', mouseOverMe());
     
-    $('h1').bind('click', mouseClick);
+ //   $('h1').bind('click', mouseClick);
     
-    $('#replaceWHtml').bind('click', replaceWHtml);
+//    $('#replaceWHtml').bind('click', replaceWHtml);
     $('#replaceWText').bind('click', replaceWText);
-    $('#addAPara').bind('click', addAPara);
+   $('#addAPara').bind('click', addAPara);
     $('#removeAPara').bind('click', removeAPara);
+//    $('#lastIsFirst').bind('click', lastIsFirst);
+//    $('#addBefore').bind('click', addBefore);
+//    $('#addAfter').bind('click', addAfter);
+   // $('#addToTextBox').bind('click', lasstIsFirst);
     
 });
+function addToTextBox (){
+    $('#randText').val('Random Text');
+}
+
+function addAfter(){
+    $('#randPara p:last').before('i go after anything');
+    }
+function addBefore(){
+    $('#randPara p:first').before('i go before anything');
+}
+
+function lastIsFirst(){
+    $('#randAPara p:last').append($('#randPara p:first'));
+}
+
+function addAPara(){
+    $('#randpara').append('<p>Another paragraph</p>');
+}
+
 function replaceWText(){
     $('#h3Tag').html('<h6>Now I\'m an h6</h6>');
 }
